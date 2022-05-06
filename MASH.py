4677d505076f19593040eb6f156656499b264e69
Kate B <101562743+mkb08002@users.noreply.github.com>
2022-05-06 09:46:42 -0400

    structuring place_to_live list

--- MASH.py
+++ MASH.py
@@ -2,22 +2,24 @@
 
 def place_to_live():
     places_to_live = []
-    places_to_live = input("Please type one place you would like to live: ")
+    
+    if len(places_to_live) < 2:
+        places_to_live = input("Please type one place you would like to live: ")
+        for place in places_to_live:
+            if place not in player_1['live']:
+                player_1['live'] = player_1['live'].append(places)
+            else:
+                print("You have already submitted this place. ")
+    print(player_1)
 
-    if places_to_live not in player_1['live']:
-        player_1['live'] = player_1['live'].append(places_to_live)
-    else:
-        print("You have already submitted this place. ")
-
-def two_players():
+def play_the_game():
     player_1 = {
         'live':'',
         'marry':'',
         'job':'' }
+    
+    place_to_live()
 
-    if player_1['live'] == False:
-        place_to_live()
-        print(player_1)
-
+play_the_game()
 
 # begin draw swirl/tally marks - button to stop drawing - count swirl lines/tally marks - apply count to lists - cross off item after each count until 1 item on each list remaining
