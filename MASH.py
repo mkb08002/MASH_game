926f8351db61beddcb99203326faef0b2f14a057
Kate B <101562743+mkb08002@users.noreply.github.com>
2022-05-06 16:56:05 -0400

    building list functions complete

--- MASH.py
+++ MASH.py
@@ -9,23 +9,37 @@ with open('locations.txt') as location_list:
     locations = location_list.read().splitlines()
 locations_random = random.choice(locations)
 
+with open('careers.txt') as careers_list:
+    careers = careers_list.read().splitlines()
+careers_random = random.choice(careers)
+
 player_1 = {
+    'MASH':['Mansion', 'Apartment', 'Shack', 'House'],
     'live':'',
     'marry':'',
     'job':'' }
 
 def play_the_game():
-    
+    print('''
+    You will be prompted to submit 3 places to live, 3 people to marry, and 3 careers. 
+    Make sure they are all unique!''')
+
     place_to_live()
     person_to_marry()
-    # career()
+    career()
+
+    print("Here is your MASH list: ")
+    for item in player_1.items():
+        print(item)
+    # print(player_1.items())
+
 
 def place_to_live():
     places_to_live = []
     player_1['live'] = places_to_live
     
-    #ask for input on 2 values for places to live - check list for 2 values, prompt question if not 2 values
-    while len(places_to_live) < 2:
+    #ask for input on 3 values for places to live - check list for 2 values, prompt question if not 2 values
+    while len(places_to_live) < 3:
         places_input = input("Please type one place you would like to live: ")
         if places_input not in places_to_live:
             places_to_live.append(places_input)
@@ -33,14 +47,14 @@ def place_to_live():
             print("You have already submitted this place. ")
     
     places_to_live.append(locations_random)
-    print("The computer has randomly selected [" + locations_random + "] as your third place to live.")
+    print("The computer has randomly selected [" + locations_random + "] as your third place to live.\n")
 
 def person_to_marry():
     person_to_marry = []
     player_1['marry'] = person_to_marry
     
     #ask for input on 2 values for person to marry - check list for 2 values, prompt question if not 2 values
-    while len(person_to_marry) < 2:
+    while len(person_to_marry) < 3:
         marry_input = input("Please type the name of one person you would like to marry: ")
         if marry_input not in person_to_marry:
             person_to_marry.append(marry_input)
@@ -48,8 +62,22 @@ def person_to_marry():
             print("You have already submitted this person. ")
 
     person_to_marry.append(characters_random)
-    print("The computer has randomly selected [" + characters_random + "] as your third person to marry.")
+    print("The computer has randomly selected [" + characters_random + "] as your third person to marry.\n")
 
+def career():
+    future_career = []
+    player_1['job'] = future_career
+    
+    #ask for input on 2 values for person to marry - check list for 2 values, prompt question if not 2 values
+    while len(future_career) < 3:
+        career_input = input("Please type a future career: ")
+        if career_input not in future_career:
+            future_career.append(career_input)
+        else:
+            print("You have already submitted this job. ")
+
+    future_career.append(careers_random)
+    print("The computer has randomly selected [" + careers_random + "] as your third career.\n")
 
 play_the_game()
 
