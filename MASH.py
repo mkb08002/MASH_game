2ae214605edc88ba0e9f636be287119426d3c33b
Kate B <101562743+mkb08002@users.noreply.github.com>
2022-05-06 11:23:17 -0400

    adding places to live in a list - complete

--- MASH.py
+++ MASH.py
@@ -1,24 +1,27 @@
 #ask for input on 2 values for places to live - check list for 2 values, prompt question if not 2 values
+player_1 = {
+    'live':'',
+    'marry':'',
+    'job':'' }
+
+def play_the_game():
+    
+    place_to_live()
+    # person_to_marry()
+    # career()
 
 def place_to_live():
     places_to_live = []
-    
-    if len(places_to_live) < 2:
-        places_to_live = input("Please type one place you would like to live: ")
-        for place in places_to_live:
-            if place not in player_1['live']:
-                player_1['live'] = player_1['live'].append(places)
-            else:
-                print("You have already submitted this place. ")
-    print(player_1)
+    player_1['live'] = places_to_live
 
-def play_the_game():
-    player_1 = {
-        'live':'',
-        'marry':'',
-        'job':'' }
     
-    place_to_live()
+    while len(places_to_live) < 2:
+        places_input = input("Please type one place you would like to live: ")
+        if places_input not in places_to_live:
+            places_to_live.append(places_input)
+        else:
+            print("You have already submitted this place. ")
+    print(player_1)
 
 play_the_game()
 
